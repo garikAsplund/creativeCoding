43b8747ba61ad498bf646c0828a119935343c949
Garik Asplund <garik.asplund@gmail.com>
2023-06-23 14:27:35 -0700

    added tweak pane and cleaned up code

--- sketches/sketch99.js
+++ sketches/sketch99.js
@@ -1,35 +1,55 @@
 const canvasSketch = require('canvas-sketch');
+const Tweakpane = require('tweakpane');
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true
 };
 
+const params = {
+  rows: 10,
+  radius: 5,
+  r: 0.02,
+  g: 0.02,
+  b: 0.02,
+  a: 0.5,
+  rate: 2.0,
+}
+
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
-    const rows = 99;
-    const radius = 1;
+    const rows = params.rows;
+    const radius = params.radius;
+    const gridw = width * 0.8;
+    const gridh = height * 0.8;
+    const margx = (width - gridw) / 2;
+    const margy = (height - gridh) / 2;
 
     for (let i = 0; i < rows; i++) {
       const columns = i + 1;
-      const y = ((height - radius) / (rows + 1)) * (i + 1);
-      const gap = width / (rows + 1);
+      const y = ((gridh - radius) / (rows + 1)) * (i + 1);
+      const gap = gridw / (rows + 1);
 
       const y2 = (Math.sqrt(3) / 2) * gap * i + gap;
 
       for (let j = 0; j < columns; j++) {
-        const x = gap * j + (width / 2) - (gap * i / 2);
+        const x = gap * j + (gridw / 2) - (gap * i / 2);
 
         context.save();
+        // const n = random.noise3D(x, y, f * 10, params.freq);
+
 
         context.fillStyle = `rgb(
-          ${Math.floor(255 - 4 * j)},
-          ${Math.floor(255 - 7 * i)},
-            122, ${0.9 * (i + 1)}
+          ${Math.abs(255 - params.r * j / params.rate  - frame * 2) % 256},
+          ${Math.abs(255 - params.g * i / params.rate - frame * 3) % 256},
+          ${Math.abs(255 - params.b * -j / params.rate - frame * 3) % 256}, 
+          ${params.a * 0.5 * (i + 1)}
         )`;
 
+        context.translate(margx, margy);
         context.beginPath();
         context.arc(x, y2, radius + (radius / rows ) * i , 0, Math.PI * 2);
         context.fill();
@@ -40,4 +60,19 @@ const sketch = () => {
   };
 };
 
+const createPane = () => {
+  const pane = new Tweakpane.Pane();
+  let folder;
+
+  folder = pane.addFolder({ title: 'Triangle' });
+  folder.addInput(params, 'rows', {min: 1, max: 222, step: 1});
+  folder.addInput(params, 'radius', {min: 1, max: 60, step: 1});
+  folder.addInput(params, 'r', {min: 0, max: 256, step: 1});
+  folder.addInput(params, 'g', {min: 0, max: 256, step: 1});
+  folder.addInput(params, 'b', {min: 0, max: 256, step: 1});
+  folder.addInput(params, 'a', {min: 0, max: 1, step: 0.01});
+  folder.addInput(params, 'rate', {min: 0.1, max: 4, step: 0.1});
+}
+
+createPane();
 canvasSketch(sketch, settings);
