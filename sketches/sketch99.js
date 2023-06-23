7bce6eb4d0c9646ff2436a525c73f992b227c28b
Garik Asplund <garik.asplund@gmail.com>
2023-06-22 20:25:37 -0700

    made equilateral

--- sketches/sketch99.js
+++ sketches/sketch99.js
@@ -9,15 +9,17 @@ const sketch = () => {
     context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
-    const rows = 100;
+    const rows = 5;
     const radius = 2;
 
     for (let i = 0; i < rows; i++) {
       const columns = i + 1;
       const y = ((height - radius) / (rows + 1)) * (i + 1);
+      const gap = width / (rows + 1);
+
+      const y2 = (Math.sqrt(3) / 2) * gap * i + gap;
 
       for (let j = 0; j < columns; j++) {
-        const gap = width / (rows + 1);
         const x = gap * j + (width / 2) - (gap * i / 2);
 
         context.save();
@@ -25,7 +27,7 @@ const sketch = () => {
         context.fillStyle = 'white';
 
         context.beginPath();
-        context.arc(x, y, radius, 0, Math.PI * 2);
+        context.arc(x, y2, radius, 0, Math.PI * 2);
         context.fill();
 
         context.restore();
