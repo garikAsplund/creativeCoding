0094cb353c93afb033a32240c573cd100d633fed
Garik Asplund <garik.asplund@gmail.com>
2023-11-06 12:04:18 -0800

    pushing particles

--- sketches/sketch-particles.js
+++ sketches/sketch-particles.js
@@ -6,9 +6,14 @@ const settings = {
 };
 
 const particles = [];
+const cursor = { x: 9999, y: 9999 };
+let elCanvas;
 
-const sketch = ({ width, height }) => {
+const sketch = ({ width, height, canvas }) => {
   let x, y, particle;
+
+  elCanvas = canvas;
+  canvas.addEventListener('mousedown', onMouseDown)
 
   for (let i = 0; i< 1; i++) {
     x = width * 0.5;
@@ -30,6 +35,31 @@ const sketch = ({ width, height }) => {
   };
 };
 
+const onMouseDown = (e) => {
+  window.addEventListener('mousemove', onMouseMove);
+  window.addEventListener('mouseup', onMouseUp);
+
+  onMouseMove(e);
+}
+
+const onMouseMove = (e) => {
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+
+  cursor.x = x;
+  cursor.y = y;
+
+  console.log({ cursor }); 
+}
+
+const onMouseUp = () => {
+  window.removeEventListener('mousemove', onMouseMove);
+  window.removeEventListener('mouseup', onMouseUp);
+
+  cursor.x = 9999;
+  cursor.y = 9999;
+}
+
 canvasSketch(sketch, settings);
 
 class Particle {
@@ -47,13 +77,38 @@ class Particle {
     this.iy = y;
 
     this.radius = radius;
+
+    this.minDist = 100;
+    this.pushFactor = 0.02;
+    this.pullFactor = 0.004;
+    this.dampFactor = 0.95;
   }
 
   update() {
-    this.ax += 0.001;
+    let dx, dy, dd, distDelta;
+
+    dx = this.ix - this.x;
+    dy = this.iy - this.y;
+
+    this.ax = dx * this.pullFactor;
+    this.ay = dy * this.pullFactor;
+
+    dx = this.x - cursor.x;
+    dy = this.y - cursor.y;
+    dd = Math.sqrt(dx * dx + dy * dy);
+
+    distDelta = this.minDist - dd;
+
+    if (dd < this.minDist) {
+      this.ax += (dx / dy) * distDelta * this.pushFactor;
+      this.ay += (dy / dd) * distDelta * this.pushFactor;
+    }
 
     this.vx += this.ax;
     this.vy += this.ay;
+
+    this.vx *= this.dampFactor;
+    this.vy *= this.dampFactor;
 
     this.x += this.vx;
     this.y += this.vy;
